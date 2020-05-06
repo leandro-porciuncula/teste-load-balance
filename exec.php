68b53e80716e2b017e2b624134d6e434b8cde2e9
Leandro Porciuncula <leandro@initd.com.br>
2020-05-06 10:32:56 -0300

    primeira versao

---
+++ exec.php
@@ -0,0 +1,14 @@
+<?php
+echo "teste";
+shell_exec('stress --cpu 2 &');
+?>
+    <html>
+    <title>Titulo do site</title>
+    <head>
+        <meta http-equiv="refresh" content=1;url="index.php">
+    </head>
+    <body>
+    <h1> Olá mundo </h1>
+    </body>
+    </html>
+
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+  $url = "http://169.254.169.254/latest/meta-data/instance-id";
+  $instance_id = file_get_contents($url);
+  echo " <h1> <font color='#adff2f'>  Instance ID: <b>" . $instance_id . "</b><br/></font> </h1> ";
+?>
+
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+   <HEAD>
+      <TITLE>
+         Cloud Treinamentos
+      </TITLE>
+   </HEAD>
+<BODY>
+        <br>
+        <H1>Cloud Treinamentos</H1>
+        <span style="color:red;font-size:25px">Iniciar stress processador 100% </span>
+        <a href="exec.php" target="_blank">AQUI</a>
+        <br>
+        <br>
+        <span style="color:red;font-size:25px">Iniciar stress processador 30% </span>
+       <a href="20.php" target="_blank" >AQUI</a>
+        <br>
+        <br>
+        <span style="color:red;font-size:25px">Para stress precessador </span>
+       <a href="para.php" >PARAR</a>
+        <br>
+
+</BODY>
