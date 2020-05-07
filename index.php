547a49412e29ae0ca07c01225e6ae6af3bbb8b11
leandro-porciuncula <leandro@cloudtreinamentos.com>
2020-05-07 09:32:23 -0300

    Cor azul

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $url = "http://169.254.169.254/latest/meta-data/instance-id";
   $instance_id = file_get_contents($url);
-  echo " <h1> <font color='black'>  Instance ID: <b>" . $instance_id . "</b><br/></font> </h1> ";
+  echo " <h1> <font color='blue'>  Instance ID: <b>" . $instance_id . "</b><br/></font> </h1> ";
 ?>
 
 <!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
